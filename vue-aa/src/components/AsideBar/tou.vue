ab0d43f0d77117f7bf54a251820d5c627ae8ec42
dangshaolong1999 <3092245754@qq.com>
2022-08-18 08:59:21 +0800

    '完整a卷'

---
+++ vue-aa/src/components/AsideBar/tou.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header-box">{{ this.$route.name }}</div>
+</template>
+
+
+<script>
+export default {
+  created() {
+    console.log(this.$route);
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.header-box {
+  height: 40px;
+  line-height: 40px;
+  padding-left: 15px;
+  border-bottom: 1px solid #ccc;
+  font-size: 14px;
+}
+</style>
